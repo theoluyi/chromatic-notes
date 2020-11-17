04c24eb6533fb387b11ae2d03bbc042da89c993d
github username <theoluyi@gmail.com>
2020-11-17 09:37:27 -0500

    creates split screen functionality w/bootstrap tutorial https://www.youtube.com/watch?v=bh3UAetYkUI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return <div>Hello this is the beginning of a brand new app</div>;
-  }
+function App() {
+  // selectedNoteIndex, selectedNote, notes
+  const [selectedNoteIndex, setSelectedNoteIndex] = useState(null);
+  const [selectedNote, setSelectedNote] = useState(null);
+  const [notes, setNotes] = useState(null);
+
+  return (
+    <>
+      <div className="row no-gutters">
+        <div className="col-md-6 no-gutters">
+          <div className="left-side d-flex justify-content-center align-items-center">
+            hello
+          </div>
+        </div>
+        <div className="col-md-6 no-gutters">
+          <div className="right-side d-flex justify-content-center align-items-center">
+            goodbye
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
