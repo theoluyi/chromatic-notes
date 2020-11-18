88f5b39a5a00dee90df01dfd965c1af7564e2b44
github username <theoluyi@gmail.com>
2020-11-18 16:09:19 -0500

    adds container fluid prop, margins smaller, more acceptable ui

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./App.css";
 import Container from "react-bootstrap/Container";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Row } from "react-bootstrap";
 
 function App() {
   // selectedNoteIndex, selectedNote, notes
@@ -10,20 +11,26 @@ function App() {
   // const [notes, setNotes] = useState(null);
 
   return (
-    <Container>
-      <div className="row">
+    <Container fluid>
+      <Row className="row">
         {/* LEFT SIDE */}
         <div className="col-md-6 no-gutters">
           <div className="left-side d-flex justify-content-center align-items-center">
-            <NotebookCard color="dark-blue" />
-            <NotebookCard color="dark-red" />
-            <NotebookCard color="dark-green" />
-            <NotebookCard color="blue" />
-            <NotebookCard color="red" />
-            <NotebookCard color="green" />
-            <NotebookCard color="light-blue" />
-            <NotebookCard color="light-red" />
-            <NotebookCard color="light-green" />
+            <Row>
+              <NotebookCard color="dark-blue" />
+              <NotebookCard color="dark-red" />
+              <NotebookCard color="dark-green" />
+            </Row>
+            <Row>
+              <NotebookCard color="blue" />
+              <NotebookCard color="red" />
+              <NotebookCard color="green" />
+            </Row>
+            <Row>
+              <NotebookCard color="light-blue" />
+              <NotebookCard color="light-red" />
+              <NotebookCard color="light-green" />
+            </Row>
           </div>
         </div>
         {/* RIGHT SIDE */}
@@ -32,7 +39,7 @@ function App() {
             right
           </div>
         </div>
-      </div>
+      </Row>
     </Container>
   );
 }
