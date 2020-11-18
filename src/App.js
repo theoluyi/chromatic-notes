2365b8ef1845a6cf6b5c166553d6680f8dbac5de
github username <theoluyi@gmail.com>
2020-11-18 10:35:09 -0500

    adds NotebookCard comp and passes hardcoded color props down to create different instances of NotebookCard.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import NotebookCard from "./Components/NotebookCard";
+// import Button from "react-bootstrap/Button";
 
 function App() {
   // selectedNoteIndex, selectedNote, notes
@@ -10,15 +12,26 @@ function App() {
 
   return (
     <>
-      <div className="row no-gutters">
+      <div className="row">
         <div className="col-md-6 no-gutters">
+
           <div className="left-side d-flex justify-content-center align-items-center">
-            hello
-          </div>
+            <NotebookCard color="dark-blue" />
+            <NotebookCard color="dark-red" />
+            <NotebookCard color="dark-green" />
+            <NotebookCard color="blue" />
+            <NotebookCard color="red" />
+            <NotebookCard color="green" />
+            <NotebookCard color="light-blue" />
+            <NotebookCard color="light-red" />
+            <NotebookCard color="light-green" />
+          </div>  {/* App.js */}
+
+          
         </div>
         <div className="col-md-6 no-gutters">
           <div className="right-side d-flex justify-content-center align-items-center">
-            goodbye
+            right
           </div>
         </div>
       </div>
---
+++ src/Components/NotebookCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NotebookCard = (props) => {
+  return <div id={props.color}> inside the NotebookCard</div>;
+};
+
+export default NotebookCard;
