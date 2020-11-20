e50360cfdd0cb3f3cb9fe1d668fb014edc36882f
github username <theoluyi@gmail.com>
2020-11-19 21:49:26 -0500

    I started trying to swap out the grid code in this file and replace it with code from xaea-12, but Im deciding its time to use gits full version control power.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import "./App.css";
-import Container from "react-bootstrap/Container";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Row } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function App() {
   // selectedNoteIndex, selectedNote, notes
@@ -11,41 +12,74 @@ function App() {
   // const [notes, setNotes] = useState(null);
 
   return (
-    <Container fluid>
-      <Row className="row">
-        {/* LEFT SIDE */}
-        <div className="col-md-6">
-          <div className="left-side d-flex justify-content-center align-items-center">
-            <Row>
-              <NotebookCard color="dark-blue" />
-              <NotebookCard color="dark-red" />
-              <NotebookCard color="dark-green" />
-            </Row>
-            <Row>
-              <NotebookCard color="blue" />
-              <NotebookCard color="red" />
-              <NotebookCard color="green" />
-            </Row>
-            <Row>
-              <NotebookCard color="light-blue" />
-              <NotebookCard color="light-red" />
-              <NotebookCard color="light-green" />
-            </Row>
-          </div>
-        </div>
-        {/* RIGHT SIDE */}
-        <div className="col-md-6">
-          <div className="right-side d-flex justify-content-center align-items-center">
-            right
-          </div>
-        </div>
-      </Row>
-    </Container>
+    <div className="notebook-grid">
+      <NotebookCard color="dark-blue" />
+      <NotebookCard color="dark-red" />
+      <NotebookCard color="dark-green" />
+      <NotebookCard color="blue" />
+      <NotebookCard color="red" />
+      <NotebookCard color="green" />
+      <NotebookCard color="light-blue" />
+      <NotebookCard color="light-red" />
+      <NotebookCard color="light-green" />
+    </div>
   );
 }
 
 const NotebookCard = (props) => {
   return <div id={props.color}> inside the NotebookCard</div>;
 };
+
+{
+  /* <Container fluid>
+      <Row className="row">
+        <div className="col-md-6">
+          <div className="left-side d-flex justify-content-center align-items-center">
+            <div className="test">
+              <Row>
+                <Col>
+                  <NotebookCard className="notebook-card" color="dark-blue" />
+                </Col>
+                <Col>
+                  <NotebookCard className="notebook-card" color="dark-red" />
+                </Col>
+                <Col>
+                  <NotebookCard className="notebook-card" color="dark-green" />
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <NotebookCard className="notebook-card" color="blue" />
+                </Col>
+                <Col>
+                  <NotebookCard className="notebook-card" color="red" />
+                </Col>
+                <Col>
+                  <NotebookCard className="notebook-card" color="green" />
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <NotebookCard className="notebook-card" color="light-blue" />
+                </Col>
+                <Col>
+                  <NotebookCard className="notebook-card" color="light-red" />
+                </Col>
+                <Col>
+                  <NotebookCard className="notebook-card" color="light-green" />
+                </Col>
+              </Row>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-6">
+          <div className="right-side d-flex justify-content-center align-items-center">
+            right
+          </div>
+        </div>
+      </Row>
+    </Container> 
+  */
+}
 
 export default App;
