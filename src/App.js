be13961fb759c4be61cfd229a65f9374686dff3a
github username <theoluyi@gmail.com>
2020-11-19 22:12:47 -0500

    deletes extra bootstrap classNames causing trouble in left and right splitscreen panels

--- src/App.js
+++ src/App.js
@@ -9,34 +9,35 @@ function App() {
   const [selectedNote, setSelectedNote] = useState(null);
   const [notes, setNotes] = useState(null);
 
+  // removed these additional bootstrap classNames from left and right-side:  d-flex justify-content-center align-items-center
   return (
-    <>
-      <div className="row">
-        <div className="col-md-6 no-gutters">
-          <div className="left-side d-flex justify-content-center align-items-center">
-            <NotebookCard color="dark-blue" />
-            <NotebookCard color="dark-red" />
-            <NotebookCard color="dark-green" />
-            <NotebookCard color="blue" />
-            <NotebookCard color="red" />
-            <NotebookCard color="green" />
-            <NotebookCard color="light-blue" />
-            <NotebookCard color="light-red" />
-            <NotebookCard color="light-green" />
-          </div>
-        </div>
-        <div className="col-md-6 no-gutters">
-          <div className="right-side d-flex justify-content-center align-items-center">
-            right
-          </div>
+    <div className="row no-gutters">
+      <div className="col-md-6 no-gutters">
+        <div className="left-side">
+          <NotebookCard color="dark-blue" />
+          <NotebookCard color="dark-red" />
+          <NotebookCard color="dark-green" />
+          <NotebookCard color="blue" />
+          <NotebookCard color="red" />
+          <NotebookCard color="green" />
+          <NotebookCard color="light-blue" />
+          <NotebookCard color="light-red" />
+          <NotebookCard color="light-green" />
         </div>
       </div>
-    </>
+      <div className="col-md-6 no-gutters">
+        <div className="right-side">right</div>
+      </div>
+    </div>
   );
 }
 
 const NotebookCard = (props) => {
-  return <div id={props.color}> inside the NotebookCard</div>;
+  return (
+    <div className="notebook-card" id={props.color}>
+      inside the NotebookCard
+    </div>
+  );
 };
 
 export default App;
