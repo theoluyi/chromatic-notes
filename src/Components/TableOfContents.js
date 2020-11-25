1504bff0cc5025aeeaaa2f7ce32edfc2f7fb9261
github username <theoluyi@gmail.com>
2020-11-25 18:28:39 -0500

    stretches TOC height to 100%

--- src/Components/TableOfContents.js
+++ src/Components/TableOfContents.js
@@ -3,12 +3,22 @@ import NotePreviewCard from "./NotePreviewCard";
 
 const TableOfContents = (props) => {
   return (
-    <div style={{ backgroundColor: "#6581e4", width: "100%" }}>
+    <div
+      style={{
+        backgroundColor: "#6581e4",
+        width: "100%",
+        height: "100%",
+        padding: "1rem",
+      }}
+    >
       <h2 style={{ textAlign: "center", color: "whitesmoke" }}>
         Table of Contents
       </h2>{" "}
       <br />
       <ul>
+        <NotePreviewCard />
+        <NotePreviewCard />
+        <NotePreviewCard />
         <NotePreviewCard />
         <NotePreviewCard />
         <NotePreviewCard />
