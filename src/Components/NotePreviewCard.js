c8e50861706d7d6b8b52b1661025bfaf7fa86973
github username <theoluyi@gmail.com>
2020-11-28 19:51:11 -0500

    removes commented out jsx in NotePreviewCard.js

--- src/Components/NotePreviewCard.js
+++ src/Components/NotePreviewCard.js
@@ -31,19 +31,4 @@ const NotePreviewCard = () => {
   );
 };
 
-// const referenceJSX = (
-//   <Card bg="transparent" text="light" style={{ width: "100%" }}>
-//     <Card.Body>
-//       <Card.Title color="light">Note Title</Card.Title>
-//       <Card.Text>
-//         Preview Text ..........................................
-//       </Card.Text>
-//       <span>
-//         <Button variant="secondary">Edit</Button>{" "}
-//         <Button variant="dark">Delete</Button>
-//       </span>
-//     </Card.Body>
-//   </Card>
-// );
-
 export default NotePreviewCard;
