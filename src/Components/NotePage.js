c6655f1955c814514ec074a2862d25e571bc2eef
github username <theoluyi@gmail.com>
2020-11-29 13:30:19 -0500

    adds hardcoded NotePage content with flex css

--- src/Components/NotePage.js
+++ src/Components/NotePage.js
@@ -2,7 +2,30 @@ import React from "react";
 import "../Component_Styles/NotePage.css";
 
 const NotePage = (props) => {
-  return <div>hello</div>;
+  return (
+    <div className="note-page">
+      <div className="note-info">
+        <h2>Note Title</h2>
+        <p>Creation Date</p>
+      </div>
+      <br />
+      <div className="note-body">
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
+        illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+        explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
+        odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
+        voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum
+        quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam
+        eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
+        voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam
+        corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
+        Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse
+        quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo
+        voluptas nulla pariatur?"
+      </div>
+    </div>
+  );
 };
 
 export default NotePage;
