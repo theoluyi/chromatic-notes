fc42bcedd88a5c4635c7b766cee58e63036b7f59
github username <theoluyi@gmail.com>
2021-01-13 21:26:47 -0500

    potentially about to break a lot so committing rn

--- src/Components/SignInView.js
+++ src/Components/SignInView.js
@@ -5,6 +5,8 @@ const SignInView = (props) => {
   return (
     <div className="notebook-card" id={props.color}>
       <p>{props.name}</p>
+      <p> wtf is this component for?</p>
+      <p>I think it's to tell the ppl to sign in please por for favor si vou ples</p>
     </div>
   );
 };
