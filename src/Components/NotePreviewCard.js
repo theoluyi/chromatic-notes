bc58a7601180b2b50325fc49e04a9b0fbd83a18e
github username <theoluyi@gmail.com>
2021-01-28 12:11:36 -0500

    committing any unsaved changes

--- src/Components/NotePreviewCard.js
+++ src/Components/NotePreviewCard.js
@@ -3,6 +3,11 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import Accordion from "react-bootstrap/Accordion";
 import "../Component_Styles/NotePreviewCard.css";
+import {
+  Link,
+  useRouteMatch,
+  useParams,
+} from "react-router-dom";
 
 // QQ: uses inline styling
 const NotePreviewCard = (props) => {
@@ -22,10 +27,13 @@ const NotePreviewCard = (props) => {
             <span className="note-title">{note_title} <br /> <small>{date_created}</small></span>{" "}
           </Accordion.Toggle>
           <span>
-            <Button variant="secondary" onClick={handleEditNoteClick}>📖</Button>{" "}
+            <Button variant="secondary" onClick={handleEditNoteClick}>
+            {/* new stuff right here with this link */}
+              <Link to={`${match.url}/xoxo`}>📖 </Link>
+            </Button>{" "}
             <Button variant="dark">🗑</Button>
           </span>
-          <span>1-10</span>
+          <span>PageNum</span>
         </Card.Header>
         <Accordion.Collapse eventKey="0">
           <Card.Body>{note_text}</Card.Body>
