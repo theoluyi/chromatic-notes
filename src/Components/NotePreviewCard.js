1d8407e96c51ca63781df0fc06dfdb952ab237e7
github username <theoluyi@gmail.com>
2021-02-04 19:05:10 -0500

    swaps match.url with url from useRouteMatch()

--- src/Components/NotePreviewCard.js
+++ src/Components/NotePreviewCard.js
@@ -3,12 +3,13 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import Accordion from "react-bootstrap/Accordion";
 import "../Component_Styles/NotePreviewCard.css";
-import {Link} from "react-router-dom";
+import {Link, useRouteMatch} from "react-router-dom";
 
 // QQ: uses inline styling
 const NotePreviewCard = (props) => {
   const {date_created, note_text, note_title} = props.noteInfo;
   const {match} = props;
+  const {url} = useRouteMatch()
 
   // const handleButtonClick = ( ) => {
   //   props.selectNote(props.noteInfo);
@@ -23,9 +24,9 @@ const NotePreviewCard = (props) => {
             <span className="note-title">{note_title} <br /> <small>{date_created}</small></span>{" "}
           </Accordion.Toggle>
           <span>
-            <Button variant="secondary" onClick={handleButtonClick}>
+            <Button variant="secondary">
             {/* new stuff right here with this link */}
-              <Link to={`${match.url}/${props.id}`}>📖 </Link>
+              <Link to={`${url}/${props.id}`}>📖 </Link>
             </Button>{" "}
             <Button variant="dark">🗑</Button>
           </span>
