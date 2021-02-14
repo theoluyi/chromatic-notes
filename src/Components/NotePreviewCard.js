5f1a804fe72cc516c69c0e9e3fa4a918b29052bc
github username <theoluyi@gmail.com>
2021-02-14 15:37:45 -0500

    cleans up date text in NotePreviewCard

--- src/Components/NotePreviewCard.js
+++ src/Components/NotePreviewCard.js
@@ -10,12 +10,15 @@ const NotePreviewCard = (props) => {
   const {date_created, note_text, note_title} = props.noteInfo;
   const {url} = useRouteMatch()
 
+  console.log("date_created ", date_created)
+  const prettyDate = (date_created.slice(0,9))
+
   return (
     <Accordion>
       <Card bg="transparent" text="light">
         <Card.Header>
           <Accordion.Toggle as={Button} variant="transparent" eventKey="0">
-            <span className="note-title">{note_title} <br /> <small>{date_created}</small></span>{" "}
+            <span className="note-title">{note_title} <br /> <small>{prettyDate}</small></span>{" "}
           </Accordion.Toggle>
           <span>
           <Link to={`${url}/${props.id+1}`} >
@@ -25,7 +28,7 @@ const NotePreviewCard = (props) => {
           </Link>
             <Button variant="dark">🗑</Button>
           </span>
-          <span>PageNum</span>
+          {/* <span>PageNum</span> */}
         </Card.Header>
         <Accordion.Collapse eventKey="0">
           <Card.Body>{note_text}</Card.Body>
