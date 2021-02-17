eb72c5e4167b19c1a3a2731e6991fda0666c490d
github username <theoluyi@gmail.com>
2021-02-17 16:12:29 -0500

    adds back button and adjusts css

--- src/Components/NotePage.js
+++ src/Components/NotePage.js
@@ -1,7 +1,9 @@
 import React from "react";
 import "../Component_Styles/NotePage.css";
 import RichEditor from "./RichEditor";
-import {useParams} from "react-router-dom"
+import {useParams, useHistory} from "react-router-dom"
+import Button from "react-bootstrap/Button";
+
   // I want to see if we can get params in TOC instead of here.
   // Then we can just filter and render a single Route there... Not sure
   // if that saves many lines of code though but.... 
@@ -9,6 +11,7 @@ import {useParams} from "react-router-dom"
 const NotePage = (props) => {
   const {color} = props;
   const {noteId} = useParams();
+  const history = useHistory();
 
   const myNote = props.notes.find( ({note_id}) => note_id === Number(noteId));
   const {date_created, note_text, note_title} = myNote;
@@ -16,9 +19,14 @@ const NotePage = (props) => {
 
   return (
     <div className={`note-page ${color}`}>
-      <div className="note-info">
         <h2>{note_title}</h2>
-        <p>Created: {prettyDate}</p>
+        
+        <div className="note-info">
+        <Button variant="transparent" 
+                onClick={() => history.goBack()}>
+                Back to Table of Contents
+        </Button>
+        <span>Created: {prettyDate}</span>
       </div>
       <div className="note-body">
         <RichEditor note_text={note_text}/>
