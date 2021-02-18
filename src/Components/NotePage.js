353cdc48396590adfe8bd933d2e704abcdcc1149
github username <theoluyi@gmail.com>
2021-02-17 19:57:20 -0500

    handles NotePage refresh error

--- src/Components/NotePage.js
+++ src/Components/NotePage.js
@@ -13,8 +13,14 @@ const NotePage = (props) => {
   const {noteId} = useParams();
   const history = useHistory();
 
+  const errorMessage = {
+    date_created: "Today",
+    note_text: "Notes in this app are generated with randomly assigned id numbers, which also determine the url/endpoint of a note. You probably got here by refreshing while looking at a note. That note no longer exists now, but the url in your browser's address bar is still pointed to it. I could probably fix this with a simple redirect, but I find this error amusing.",
+    note_title: "This note no longer exists, ah darnit."
+  }
+
   const myNote = props.notes.find( ({note_id}) => note_id === Number(noteId));
-  const {date_created, note_text, note_title} = myNote;
+  const {date_created, note_text, note_title} = myNote || errorMessage;
   const prettyDate = (date_created.slice(0,9))
 
   return (
