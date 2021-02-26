f157b89a9d8406ae5b81854b3a2ccdef90ea1e3a
github username <theoluyi@gmail.com>
2021-02-25 19:34:24 -0500

    comments back in useless stuff because its catching errors that break the app

--- src/Components/NotePage.js
+++ src/Components/NotePage.js
@@ -21,17 +21,14 @@ const NotePage = (props) => {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-  // const errorMessage = {
-  //   date_created: "Today",
-  //   note_text: `Notes in this app are generated with randomly assigned id numbers, which also determine the url/endpoint of a note (e.g., this one's is ${noteId}). You probably got here by refreshing while looking at a note. That note no longer exists now, but the url in your browser's address bar is still pointed to it. I could probably fix this with a simple redirect, but I find this error amusing and educating.`,
-  //   note_title: "This note no longer exists 🥸 ah darnit"
-  // }
+  const errorMessage = {
+    date_created: "Today",
+    note_text: `Notes in this app are generated with randomly assigned id numbers, which also determine the url/endpoint of a note (e.g., this one's is ${noteId}). You probably got here by refreshing while looking at a note. That note no longer exists now, but the url in your browser's address bar is still pointed to it. I could probably fix this with a simple redirect, but I find this error amusing and educating.`,
+    note_title: "This note no longer exists 🥸 ah darnit"
+  }
 
   const myNote = props.notes.find( ({note_id}) => note_id === Number(noteId));
-  const {date_created, note_text, note_title} = (
-    myNote 
-    // || errorMessage
-  )
+  const {date_created, note_text, note_title} = myNote || errorMessage;
   const prettyDate = (date_created.slice(0,9))
 
   if (myNote) { return (
