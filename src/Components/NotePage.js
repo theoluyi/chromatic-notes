3464b318b50780a828aff81cceeeb7ac131b14e5
github username <theoluyi@gmail.com>
2021-02-25 19:38:29 -0500

    makes here the target of click at level of UI/UX

--- src/Components/NotePage.js
+++ src/Components/NotePage.js
@@ -68,7 +68,7 @@ const NotePage = (props) => {
     )
   } else {
     console.log('history :>> ', history);
-    return <div onClick={() => history.goBack()} style={{color: "black"}}> This note no longer exists, please select a notebook from the left to see available notes, or click here to go back.</div>
+    return <div onClick={() => history.goBack()} style={{color: "black"}}> This note no longer exists, please select a notebook from the left to see available notes, or click <big><strong>here</strong></big> to go back.</div>
   }
 };
 
